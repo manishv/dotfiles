fb4d9a4ceb9b73d0e39678c47b66e658913278e0
Manish Verma <manish.verma@arm.com>
2011-11-13 22:09:45 +0000

    adding python lib file

---
+++ emacs/lisp-personal/mpv-python.el
@@ -0,0 +1,17 @@
+;; Customizations for python programming
+(defun todo-highlight ()
+  (font-lock-add-keywords nil
+			  '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t))))
+
+(load-library "autopair")
+
+(defun my-python-mode-hook()
+  (todo-highlight) ;;high the above keywords
+  (which-function-mode t)
+  (if (> emacs-major-version 22)
+      (linum-mode t))
+  (autopair-mode)
+  )
+
+(add-hook 'python-mode-hook 'my-python-mode-hook)
+(provide 'mpv-python)
