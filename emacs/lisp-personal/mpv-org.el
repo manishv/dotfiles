6e8418b6ed65e4e69858b9482f1fafe59ae3d3ce
Manish Verma <manish.verma@arm.com>
2012-10-23 09:28:09 +0100

    Updated configuration

--- emacs/lisp-personal/mpv-org.el
+++ emacs/lisp-personal/mpv-org.el
@@ -2,16 +2,13 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (setq org-log-done t)
 
-(require 'remember)
-(setq remember-annotation-functions '(org-remember-annotation))
-(setq remember-handler-functions '(org-remember-handler))
-(add-hook 'remember-mode-hook 'org-remember-apply-template)
-(define-key global-map [(control meta ?r)] 'remember)
+;; (require 'remember)
+;; (setq remember-annotation-functions '(org-remember-annotation))
+;; (setq remember-handler-functions '(org-remember-handler))
+;; (add-hook 'remember-mode-hook 'org-remember-apply-template)
+;; (define-key global-map [(control meta ?r)] 'remember)
 
 
-(setq org-agenda-files
-      (list "J:/Work/Tasks/work.org" "J:/Work/Tasks/tasks.org" ))
-(setq org-default-notes-file "J:/Work/Tasks/notes.org")
 (setq org-deadline-warning-days 14)
 (setq org-agenda-show-all-dates t)
 (setq org-agenda-skip-deadline-if-done t)
@@ -20,8 +17,13 @@
 (setq org-reverse-note-order t)
 (setq org-fast-tag-selection-key t)
 (setq org-use-fast-todo-selection t)
-(setq org-remember-templates
-      '(("Todo" ?t "* TODO %? \nAdded: %U" "J:/Work/Tasks/tasks.org" "TASKS")
-	("Note" ?n "* %U %?\n %i " "J:/Work/Tasks/notes.org")))
+
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(local-unset-key (quote [S-down]))
+(local-unset-key (quote [S-up]))
+(setq org-log-done t)
+(setq org-agenda-files (list "~/work/tasks/office-tasks.org"
+			     "~/work/tasks/personal-tasks.org"))
 
 (provide 'mpv-org)
