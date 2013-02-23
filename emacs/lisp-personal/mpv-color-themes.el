c1290291698cc5140d1e3e995b6e57ce5b23bd38
Manish Verma <manish.verma79@gmail.com>
2013-02-23 19:24:56 +0000

    Removed unused themes and stop loading

--- emacs/lisp-personal/mpv-color-themes.el
+++ emacs/lisp-personal/mpv-color-themes.el
@@ -2,7 +2,7 @@
 (require 'color-theme)
 (load-library "tango")
 (load-library "color-theme-tangotango")
-(load-library "zenburn")
+;(load-library "zenburn")
 (color-theme-initialize)
 
 (provide 'mpv-color-themes)
